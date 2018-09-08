cb89b956aa61dda857e63a52c91904e1159144a5
Samson Fong <shfong@ucsd.edu>
2018-09-08 10:53:03 -0700

    Initial Flask app

---
+++ app/upload.py
@@ -0,0 +1,11 @@
+import functools
+
+from flask import (
+    Blueprint, flash, g, redirect, render_template, request, session, url_for
+)
+from werkzeug.security import check_password_hash, generate_password_hash
+
+from flaskr.db import get_db
+
+bp = Blueprint('upload', __name__, url_prefix='/upload')
+
---
+++ run_app.sh
@@ -0,0 +1,4 @@
+export FLASK_APP=app
+export FLASK_ENV=development
+
+flask run
